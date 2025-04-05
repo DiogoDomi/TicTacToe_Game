423f500c1337e6560e0e4725c535a6f76bac0f6d
Diogo <diogodomidacruz@gmail.com>
2025-04-04 21:55:46 -0300

    Arrumando todos os bugs que estavam dando

--- main.c
+++ main.c
@@ -1,95 +1,191 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
-
-#define LINES 3
-#define COLS 3
+#include <stdint.h>
+#include <unistd.h>
 
 typedef struct Board {
-    char _Board[LINES][COLS];
+    char _Board[3][3];
 } Board;
 
 typedef struct Player {
-    char *Name;
+    char Name[15];
     char Piece;
-    int Win, Loss, Tie;
+    uint8_t Win;
+    uint8_t Loss;
+    uint8_t Tie;
 
 } Player;
 
 typedef struct Position {
-    int X, Y;
+    uint8_t X;
+    uint8_t Y;
 } Position;
 
-typedef struct Match {
-    Player Player1, Player2;
+typedef struct Game {
     Board Board;
-    Position Pos;
+    Player Player1, Player2;
+    Position Position;
 
-    unsigned short int Round, Wins, Ties, Loss;
+    uint8_t Round;
+    uint8_t Moves;
+    uint8_t Wins;
+    uint8_t Ties;
+    uint8_t Loss;
+    uint8_t MAX_MOVES;
     bool isPlayer1Turn;
     bool isPlayer2Turn;
-    bool roundIsFinished;
+    bool someoneWon;
+    bool endRound;
+    bool endGame;
 
-} Match;
+} Game;
 
 void getData(Player *player1, Player *player2); 
-void printBoard(Board *board);
+void printBoard(Board board);
 void cleanBoard(Board *board); 
-void insertPiece(Board *board, Player *player, Position pos);
+void insertPiece(Game *game);
+void initRound(Game *game);
 
 int main() {
+    
+    Board board;
+    Player player1, player2;
+    Position pos = {
+        .X = 0,
+        .Y = 0
+    };
+    Game game = {
+        .Board = board,
+        .Player1 = player1,
+        .Player2 = player2,
+        .Position = pos,
+        .Round = 0,
+        .Moves = 0,
+        .Wins = 0,
+        .Ties = 0,
+        .Loss = 0,
+        .MAX_MOVES = 9,
+        .isPlayer1Turn = true,
+        .isPlayer2Turn = false,
+        .someoneWon = false,
+        .endRound = false,
+        .endGame = false
+    };
+
+    getData(&game.Player1, &game.Player2);
+    initRound(&game);
+
+    while (game.endGame) {
+        insertPiece(&game);
+        printBoard(game.Board);
+
+    };
     return 0;
 }
 
-/*
-bool checkIfSomeoneWon(Table *table) {
-    int rowWin[] = {{(0,0), (0,1), (0,2)}, {(1,0), (1,1), (1,2)}, {(2,0), (2,1), (2,2)}};
-    int columnWin[] = {{(0,0), (1,0), (2,0)}, {(0,1), (1,1), (2,1)}, {(0,2), (1,2), (2,2)}};
-    int diagonalWin[] = {{(0,0), (1,1), (2,2)}, {(0,2), (1,1), (2,0)}};
+bool checkIfSomeoneWon(Game *game) {
+    uint8_t rowWin[] = {{(0,0), (0,1), (0,2)}, {(1,0), (1,1), (1,2)}, {(2,0), (2,1), (2,2)}};
+    uint8_t columnWin[] = {{(0,0), (1,0), (2,0)}, {(0,1), (1,1), (2,1)}, {(0,2), (1,2), (2,2)}};
+    uint8_t diagonalWin[] = {{(0,0), (1,1), (2,2)}, {(0,2), (1,1), (2,0)}};
 
-    for (int i = 0; i < LINES; i++) {
-        for (int j = 0; j < COLS; j++) {
+    uint8_t sizeOfRow = size_of(rowWin) / sizeof(uint8_t);
+    uint8_t sizeOfCol = size_of(columnWin) / sizeof(uint8_t);
+    uint8_t sizeOfDiag = size_of(diagonalWin) / sizeof(uint8_t);
 
+    uint8_t player1Moves;
+    uint8_t player2Moves;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (game->Board._Board[i][j] == game->Player1.Piece) {
+                player1Moves++;
+            } else if (game->Board._Board[i][j] == game->Player2.Piece) {
+                player2Moves++;
+            }
         }
     }
-}
-*/
 
-void insertPiece(Board *board, Player *player, Position pos) {
-    if ( (pos.X < 1 && pos.X > LINES) || (pos.Y < 1 && pos.Y > COLS) ) {
+
+    // TERMINAR ESSA FUNÇÃO DO CARAIO
+
+
+
+
+
+
+
+}
+
+void initRound(Game *game) {
+    game->endRound = false;
+    cleanBoard(&game->Board);
+    
+    printf("Loading game...\n");
+    sleep(5);
+
+    printBoard(game->Board);
+
+    game->Round++;
+}
+
+void insertPiece(Game *game) {
+    if (game->isPlayer1Turn) {
+        printf("Your turn, Player 1.\n");
+    } else {
+        printf("Your turn, Player 2.\n");
+    }
+
+    printf("Put a piece on the table. Choose (X Y): ");
+    scanf("%hhu %hhu", &game->Position.X, &game->Position.Y);
+
+    if ( (game->Position.X < 1 && game->Position.X > 3) || (game->Position.Y < 1 && game->Position.Y > 3) ) {
         printf("Position not allowed. Try again.\n");
         return;
     }
-    if (board->_Board[pos.X - 1][pos.Y - 1] != '-') {
+    if (game->Board._Board[game->Position.X - 1][game->Position.Y - 1] != ' ') {
         printf("This Position already has a piece. Choose another place.\n");
         return;
     }
-    board->_Board[pos.X - 1][pos.Y - 1] = player->Piece;
+
+    if (game->isPlayer1Turn) {
+        game->Board._Board[game->Position.X - 1][game->Position.Y - 1] = game->Player1.Piece;
+        game->isPlayer1Turn = false;
+        game->isPlayer2Turn = true;
+    } else {
+        game->Board._Board[game->Position.X - 1][game->Position.Y - 1] = game->Player2.Piece;
+        game->isPlayer1Turn = true;
+        game->isPlayer2Turn = false;
+    }
+
+    game->Moves++;
+
 }
 
 void cleanBoard(Board *board) {
-    for (int i = 0; i < LINES; i++) {
-        for (int j = 0; j < COLS; j++) {
-            board->_Board[i][j] = '-';
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            board->_Board[i][j] = ' ';
         }
     }
 }
 
-void printTable(Board *board) {
+void printBoard(Board board) {
+    system("clear");
     printf("|----------------------------|\n");
     printf("|----- TIC-TAC-TOE GAME -----|\n");
     printf("|----------------------------|\n");
 
-    for (int i=0; i<LINES; i++){
+    for (int i=0; i<3; i++){
         if (i > 0) {
             printf("|         ---|---|---        |\n");
         }
         printf("|         ");
-        for (int j=0; j<COLS; j++) {
-            if (j == COLS - 1) {
-                printf(" %c", board->_Board[i][j]);
+        for (int j=0; j<3; j++) {
+            if (j == 3 - 1) {
+                printf(" %c", board._Board[i][j]);
             } else{
-                printf(" %c |", board->_Board[i][j]);
+                printf(" %c |", board._Board[i][j]);
             }
         }
         printf("         |\n");
@@ -102,17 +198,19 @@ void printTable(Board *board) {
 void getData(Player *player1, Player *player2) {
     printf("Choose the players names.\n");
     printf("Player 1 name: ");
-    scanf("%s", player1->Name);
-    printf("\n");
+    scanf("%19s", player1->Name);
 
     printf("Player 2 name: ");
-    scanf("%s", player2->Name);
-    printf('\n');
+    scanf("%19s", player2->Name);
+    printf("\n");
 
     printf("Choose the pieces to play.\n");
-    printf("1 for 'X'\n2 for 'O': ");
+    printf("1 for 'X'\n2 for 'O'\n");
     printf("Player 1 piece: ");
-    unsigned short int option;
+
+    uint8_t option;
+    scanf("%hhu", &option);
+
     if (option == 1) {
         player1->Piece = 'X';
         player2->Piece = 'O';
@@ -120,5 +218,6 @@ void getData(Player *player1, Player *player2) {
         player1->Piece = 'O';
         player2->Piece = 'X';
     }
-    printf('\n');
+    printf("\n");
+    printf("The data is saved.\n");
 }
